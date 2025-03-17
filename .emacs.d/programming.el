0f411927c99ce1fec63155c3310e9c760f530081
Chris Simpkins <chris.simpkins@gmail.com>
2025-03-16 21:50:54 -0400

    Update eglot bindings; flymake for python-ts-mode

--- .emacs.d/programming.el
+++ .emacs.d/programming.el
@@ -30,11 +30,10 @@
 
 (use-package eglot
   :bind (:map eglot-mode-map
-              ("C-c d" . eldoc)
-              ("C-c a" . eglot-code-actions)
+              ("C-c h" . eldoc)
+              ("C-c o" . eglot-code-action-organize-imports)
               ("C-c f" . flymake-show-buffer-diagnostics)
               ("C-c r" . eglot-rename)))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Python
@@ -42,7 +41,7 @@
 ;; First, in a shell, install the langauge server.
 ;; https://github.com/python-lsp/python-lsp-server
 ;;
-;; pip3 install python-lsp-server
+;; pip3 install "python-lsp-server[all]"
 
 ;; Then, in Emacs, install the Python treesitter grammar.  This is
 ;; interactive, just accept the defaults.
@@ -55,6 +54,7 @@
               ("<f5>" . recompile)
               ("<f6>" . eglot-format))
   :hook
-  ((python-ts-mode . eglot-ensure))
+  ((python-ts-mode . eglot-ensure)
+   (python-ts-mode . flymake-mode))
   :mode (("\\.py\\'" . python-ts-mode))
 )
