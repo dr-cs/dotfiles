fa85ce0a7291ae30f97a28ab7c4fd568b39ef000
Chris Simpkins <chris.simpkins@gmail.com>
2025-10-22 11:13:19 -0400

    Remove bash config, .emacs file

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -2,14 +2,11 @@
 
 mv ~/.emacs.d ~/.emacs.d.$(date +%F)
 export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ln -sfv "$DOTFILES_DIR/.bashrc" ~
-ln -sfv "$DOTFILES_DIR/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/.editorconfig" ~
 ln -sfv "$DOTFILES_DIR/custom-zsh-functions" ~
 ln -sfv "$DOTFILES_DIR/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/.gitignore" ~
 ln -sfv "$DOTFILES_DIR/.mbsyncrc" ~
-ln -sfv "$DOTFILES_DIR/.emacs" ~
 ln -sfv "$DOTFILES_DIR/.emacs.d" ~
 ln -sfv "$DOTFILES_DIR/certs" ~
