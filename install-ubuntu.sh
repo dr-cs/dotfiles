6063bf1816808870348e183226a3e3e4dd19135d
Chris Simpkins <chris.simpkins@gmail.com>
2025-10-22 11:06:12 -0400

    Remove programming languages; update apt commands

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,35 +1,24 @@
 #!/usr/bin/env bash
 
-apt-get -y update
-apt-get -y upgrade
-apt-get -y dist-upgrade
+apt -y update
+apt -y upgrade
+apt -y dist-upgrade
 
 # Basic utilities
-apt-get -y install xserver-xorg-input-libinput
-apt-get -y install imagemagick
-apt-get -y install tree
-apt-get -y install webkit2png
-apt-get -y install graphviz
-apt-get -y install pandoc
-apt-get -y install emacs
-apt-get -y install ispell
-apt-get -y install exfat-utils
-apt-get -y install m4
-apt-get -y install zsh
-apt-get -y install keychain
-apt-get -y install texlive-full
-
-# Programming languages
-apt-get -y install openjdk-8-jdk
-apt-get -y install openjfx
-apt-get -y install ocaml
-apt-get -y install opam
+apt -y install imagemagick
+apt -y install tree
+apt -y install webkit2png
+apt -y install graphviz
+apt -y install pandoc
+apt -y install ispell
+apt -y install exfat-utils
+apt -y install zsh
+apt -y install keychain
+apt -y install texlive-full
 
 # Clean up
-sudo apt-get autoremove
-sudo apt-get autoclean
-sudo apt-get -y update
-
-curl -s "https://get.sdkman.io" | bash
+apt autoremove
+apt autoclean
+apt -y update
 
 echo install-ubuntu.sh complete.
