64f357246155ff796f7312623fe006a692b40133
Chris Simpkins <chris.simpkins@gmail.com>
2025-10-21 23:41:36 -0400

    Add .editorconfig

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -4,6 +4,7 @@ mv ~/.emacs.d ~/.emacs.d.$(date +%F)
 export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sfv "$DOTFILES_DIR/.bashrc" ~
 ln -sfv "$DOTFILES_DIR/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/.editorconfig" ~
 ln -sfv "$DOTFILES_DIR/custom-zsh-functions" ~
 ln -sfv "$DOTFILES_DIR/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/.gitconfig" ~
