3a242ba13bcb9331064b4e44d33e62818bb17288
Chris Simpkins <chris.simpkins@gmail.com>
2026-02-24 11:18:14 -0500

    Remove antigen and SDKs

--- install-general.sh
+++ install-general.sh
@@ -2,15 +2,11 @@
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Antigen package manager for zsh
-curl -L git.io/antigen > ~/.antigen.zsh
+# # SDKMAN!
+# curl -s https://get.sdkman.io | bash
+# . "$HOME/.sdkman/bin/sdkman-init.sh"
 
-# SDKMAN!
-curl -s https://get.sdkman.io | bash
-. "$HOME/.sdkman/bin/sdkman-init.sh"
-
-# Java 8
-# sdk install java 8.0.282.hs-adpt
-# Trying out the J9 implementation.  If problems, revert to HotSpot above.
-sdk install java 8.0.282.j9-adpt
-
+# # Java 8
+# # sdk install java 8.0.282.hs-adpt
+# # Trying out the J9 implementation.  If problems, revert to HotSpot above.
+# sdk install java 8.0.282.j9-adpt
