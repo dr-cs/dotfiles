ce076d789c7a35562d9603e995fc085fcafbc360
Chris Simpkins <chris.simpkins@gmail.com>
2026-03-11 23:15:20 -0400

    Remove git, add python3 stuff

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -5,20 +5,23 @@ apt -y upgrade
 apt -y dist-upgrade
 
 # Basic utilities
-apt -y install git
 apt -y install curl
 apt -y install direnv
+apt -y install exfat-utils
+apt -y install fonts-powerline
+apt -y install graphviz
 apt -y install imagemagick
+apt -y install ispell
+apt -y install keychain
+apt -y install pandoc
+apt -y install python3-full
+apt -y install python3-gpg
+apt -y install python3-pip
+apt -y install python3-venv
+apt -y install texlive-full
 apt -y install tree
 apt -y install webkit2png
-apt -y install graphviz
-apt -y install pandoc
-apt -y install ispell
-apt -y install exfat-utils
 apt -y install zsh
-apt -y install keychain
-apt -y install fonts-powerline
-apt -y install texlive-full
 
 # Clean up
 apt autoremove
